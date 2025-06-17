b62ea03ab1dea2450257e415109301f60c5b6577
woozystudio <woozysstudio23@gmail.com>
2025-06-17 15:44:43 -0600

    feat: testing script

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,11 +1,45 @@
-import { world, system } from "@minecraft/server";
+import { world } from "@minecraft/server";
+import { ActionFormData } from "@minecraft/server-ui";
 
-function mainTick() {
-  if (system.currentTick % 100 === 0) {
-    world.sendMessage("Hello starter! Tick: " + system.currentTick);
+const ui = new ActionFormData().title("Form").body("").button("button1").button("button2").button("button3");
+
+const customUi = new ActionFormData()
+  .title("Custom Form")
+  .body("")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/promo_holiday_gift_small")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_deals")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/hammer_l")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/customUi/Circle")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_hangar")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/promo_holiday_gift_small")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_deals")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/hammer_l")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/customUi/Circle")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_hangar")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/promo_holiday_gift_small")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_deals")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/hammer_l")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/customUi/Circle")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_hangar")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/promo_holiday_gift_small")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_deals")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/hammer_l")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/customUi/Circle")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_hangar")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/promo_holiday_gift_small")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_deals")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/hammer_l")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/customUi/Circle")
+  .button({ rawtext: [{ text: "\n\n\n\naaaaaa" }] }, "textures/ui/icon_hangar");
+
+world.afterEvents.itemUse.subscribe((event) => {
+  const { source, itemStack } = event;
+  switch (itemStack.typeId) {
+    case "minecraft:compass":
+      ui.show(source);
+      break;
+    case "minecraft:clock":
+      customUi.show(source);
+      break;
   }
-
-  system.run(mainTick);
-}
-
-system.run(mainTick);
+});
