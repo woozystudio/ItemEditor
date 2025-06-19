878bf46d910b4540a600cdf4d4636197f35c00ca
woozystudio <woozysstudio23@gmail.com>
2025-06-18 19:56:50 -0600

    feat: add script event for the menu

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,13 @@
-import { world } from "@minecraft/server";
+import { Player, system, world } from "@minecraft/server";
 import { showItemEditorUI } from "./mainItemEditorUI";
+
+system.afterEvents.scriptEventReceive.subscribe((event) => {
+  const { id, sourceEntity } = event;
+  switch (id) {
+    case "itemeditor:open_main_menu":
+      showItemEditorUI(sourceEntity as Player);
+  }
+});
 
 world.afterEvents.itemUse.subscribe((event) => {
   const { source, itemStack } = event;
