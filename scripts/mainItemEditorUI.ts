6f1389e4d34a98471221e4ca12d333b6fc16b0b5
woozystudio <woozysstudio23@gmail.com>
2025-06-20 17:07:53 -0600

    feat: create attributes feature

--- scripts/mainItemEditorUI.ts
+++ scripts/mainItemEditorUI.ts
@@ -1,5 +1,6 @@
 import { EntityInventoryComponent, ItemStack, Player } from "@minecraft/server";
 import { ActionFormData, ModalFormData } from "@minecraft/server-ui";
+import { MinecraftItemTypes } from "@minecraft/vanilla-data";
 
 export const mainItemEditorUI = new ActionFormData()
   .title("Item Editor")
@@ -11,10 +12,10 @@ export const mainItemEditorUI = new ActionFormData()
     ],
   })
   .button({ rawtext: [{ text: "\n\n\n\nRename" }] }, "textures/items/blaze_rod")
-  .button({ rawtext: [{ text: "\n\n\n\nSet Lore" }] }, "textures/items/map_trial_chambers");
+  .button({ rawtext: [{ text: "\n\n\n\nSet Lore" }] }, "textures/items/map_trial_chambers")
+  .button({ rawtext: [{ text: "\n\n\n\nAttributes" }] }, "textures/items/prize_pottery_sherd");
 /* .button({ rawtext: [{ text: "\n\n\n\nDelete" }] }, "textures/items/shears")
   .button({ rawtext: [{ text: "\n\n\n\nEnchant" }] }, "textures/items/spire_armor_trim_smithing_template")
-  .button({ rawtext: [{ text: "\n\n\n\nAttributes" }] }, "textures/items/prize_pottery_sherd")
   .button({ rawtext: [{ text: "\n\n\n\nSave Item" }] }, "textures/items/ender_eye")
   .button({ rawtext: [{ text: "\n\n\n\nAmount" }] }, "textures/items/bundle_lime")
   .button({ rawtext: [{ text: "\n\n\n\nRepair" }] }, "textures/items/redstone_dust"); */
@@ -23,6 +24,7 @@ export function showItemEditorUI(player: Player) {
   mainItemEditorUI.show(player).then((e) => {
     if (e.selection === 0) return renameItemMenu(player);
     if (e.selection === 1) return setLoreMenu(player);
+    if (e.selection === 2) return editAttributesMenu(player);
   });
 }
 
@@ -84,5 +86,59 @@ export function setLoreMenu(player: Player) {
     console.warn(results);
 
     inventory?.container?.getSlot(player.selectedSlotIndex).setItem(selectedItem);
+  });
+}
+
+export function editAttributesMenu(player: Player) {
+  const attributesMenu = new ActionFormData()
+    .title("Attributes")
+    .body("Select the attributes")
+    .button({ rawtext: [{ text: "\n\n\n\nCan\nPlace On" }] }, "textures/items/flow_pottery_sherd")
+    .button({ rawtext: [{ text: "\n\n\n\nCan Destroy" }] }, "textures/items/miner_pottery_sherd")
+    .button({ rawtext: [{ text: "\n\n\n\nLock in Inventory" }] }, "textures/items/prize_pottery_sherd")
+    .button({ rawtext: [{ text: "\n\n\n\nKeep on Death" }] }, "textures/items/heartbreak_pottery_sherd");
+
+  attributesMenu.show(player).then((e) => {
+    if (e.canceled) return;
+
+    const inventory = player.getComponent("inventory") as EntityInventoryComponent;
+    const selectedItem = inventory.container?.getItem(player.selectedSlotIndex) as ItemStack;
+
+    if (e.selection === 0) {
+      const canPlaceOnMenu = new ModalFormData()
+        .title("Can Place On")
+        .textField("Enter the id of the block where it can be placed.", "grass_block")
+        .textField("Enter the id of the blocks you want to remove.", "grass_block")
+        .submitButton("Apply changes");
+
+      canPlaceOnMenu.show(player).then((e) => {
+        if (e.canceled) return;
+
+        const textFieldResponse = e.formValues as (string | number | boolean)[];
+        let responseAddLine = textFieldResponse[0] as string;
+        const responseRemoveLine = textFieldResponse[1] as string;
+
+        const oldLores = selectedItem.getCanPlaceOn();
+        let results = [...oldLores, responseAddLine];
+
+        if (responseAddLine === "") results = [...oldLores];
+
+        console.warn(responseRemoveLine);
+        if (responseRemoveLine === null) return selectedItem.setCanPlaceOn(results);
+
+        const index = results.indexOf(responseRemoveLine);
+
+        if (index !== -1) {
+          results.splice(index, 1);
+        }
+
+        // const lore = results.filter((item) => item !== responseRemoveLine);
+
+        selectedItem.setCanPlaceOn(results);
+        console.warn(results);
+
+        inventory?.container?.getSlot(player.selectedSlotIndex).setItem(selectedItem);
+      });
+    }
   });
 }
